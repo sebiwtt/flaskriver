f01af97446b4d8dd12a6c056afff09ed165079ff
Sebastian Wette <sebastian.wette@web.de>
2022-09-12 15:38:05 +0200

    build the barebone class for the flaskriver package

---
+++ flaskriver/Wrapper.py
@@ -0,0 +1,18 @@
+from flask import request
+
+class FlaskRiver:
+    def __init__(self, model):
+        self.model = model
+
+    def predict(self):
+        payload = request.json
+        return self.model.predict(payload)
+
+    def train(self):
+        payload = request.json
+        self.model.learn_one(payload["features"], payload["target"])
+        return {}, 201
+    
+    def registerToApp(self, app):
+        app.add_url_rule("/predict", "predict", self.predict)
+        app.add_url_rule("/train", "train", self.train)
