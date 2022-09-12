7d4943e224f4364710d84acdebc63c3b3954b46e
Sebastian Wette <sebastian.wette@web.de>
2022-09-12 15:43:22 +0200

    cleaned code using black

--- flaskriver/Wrapper.py
+++ flaskriver/Wrapper.py
@@ -1,5 +1,6 @@
 from flask import request
 
+
 class FlaskRiver:
     def __init__(self, model):
         self.model = model
@@ -12,7 +13,7 @@ class FlaskRiver:
         payload = request.json
         self.model.learn_one(payload["features"], payload["target"])
         return {}, 201
-    
+
     def registerToApp(self, app):
         app.add_url_rule("/predict", "predict", self.predict)
         app.add_url_rule("/train", "train", self.train)
