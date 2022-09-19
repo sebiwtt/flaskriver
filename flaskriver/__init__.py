b80e02a449db86a751d24ab75abfda207420aa1f
Sebastian Wette <sebastian.wette@telekom.de>
2022-09-19 15:57:31 +0200

    resolved some more :)

--- flaskriver/__init__.py
+++ flaskriver/__init__.py
@@ -2,11 +2,7 @@
 with the online-ML library river in an easy way.
 """
 
-<<<<<<< HEAD
-__version__ = "0.0.3"
-=======
 __version__ = "0.0.4"
 
 from .ClassificationInterface import ClassificationInterface
 from .RegressionInterface import RegressionInterface
->>>>>>> dev
